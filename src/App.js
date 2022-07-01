e05a3aacd8614409af258583c32bdc01afdfa320
ykyan-vtc <90543115+ykyan-vtc@users.noreply.github.com>
2022-07-01 16:56:55 +0800

    updated sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    },
+  ]
+  
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <div className="categories-container">
+      {categories.map(({title, id}) => (
+        <div className="category-container" key={id}>
+          <div className="background-image" />
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+
+
+      ))}
+      </div>
+
   );
 }
 
